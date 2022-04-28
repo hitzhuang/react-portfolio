c38a63e090ca485fe00ddffd0e7aff98b170cb76
Hitz Huang <hitz@pjx-studios.com>
2022-04-28 11:05:25 +0800

    update axios package

---
+++ build/precache-manifest.7c8a7eda9469d3e7daf93f1c5a8d7c20.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6fe0dad6b41326ef65d156b4981835",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a94e8b1b3f4d08b1d41b",
+    "url": "/react-portfolio/static/css/main.6cceb4dc.chunk.css"
+  },
+  {
+    "revision": "2fcc55f8b5eb8b4698fa",
+    "url": "/react-portfolio/static/js/2.f10ec900.chunk.js"
+  },
+  {
+    "revision": "5dbf9415e481a23d65662fd7dec5f7a6",
+    "url": "/react-portfolio/static/js/2.f10ec900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a94e8b1b3f4d08b1d41b",
+    "url": "/react-portfolio/static/js/main.ede9fe43.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/react-portfolio/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/react-portfolio/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/react-portfolio/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/react-portfolio/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/react-portfolio/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/react-portfolio/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/react-portfolio/static/media/6.c80394c2.jpg"
+  },
+  {
+    "revision": "081996941f89b17afa59b735edf7113b",
+    "url": "/react-portfolio/static/media/bg.08199694.jpg"
+  },
+  {
+    "revision": "7b9eef77f9cd9e1b7032bc1b8fc173aa",
+    "url": "/react-portfolio/static/media/colorpalette.7b9eef77.png"
+  },
+  {
+    "revision": "9f78b7754ba6bad2032fbbb5ce471ad1",
+    "url": "/react-portfolio/static/media/dadjokes.9f78b775.png"
+  },
+  {
+    "revision": "cab77f91d69d3f3d4e5a1fe9b7b09b81",
+    "url": "/react-portfolio/static/media/hangman.cab77f91.png"
+  },
+  {
+    "revision": "fe11b0f440dc4e36ba9b4c9c21dc3a52",
+    "url": "/react-portfolio/static/media/lights_out.fe11b0f4.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
